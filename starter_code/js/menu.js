5a35db719bfba086544a38c3d76ef979e3e12d79
galle171 <jgallemi95@gmail.com>
2017-11-14 19:53:20 +0100

    almost

---
+++ starter_code/js/menu.js
@@ -0,0 +1,6 @@
+var btnMenu = document.getElementById("btn-menu");
+var nav = document.getElementById("nav");
+
+btnMenu.addEventListener('click', function(){
+    nav.classList.toggle('mostrar')
+})
