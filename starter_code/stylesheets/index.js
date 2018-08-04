04125e055cf50261fa55837bc3a60399bc513d73
vazz <elisa.merinovazquez@gmail.com>
2018-08-04 16:09:32 +0200

    mobile done

---
+++ starter_code/stylesheets/index.js
@@ -0,0 +1,19 @@
+function myFunction() {
+	var x = document.getElementById('drops');
+
+	if (x.style.display === 'block') {
+		x.style.display = 'none';
+	} else {
+		x.style.display = 'block';
+	}
+	console.log('hello');
+}
+
+/*function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}*/
