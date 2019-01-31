de0cc33f9db81ffaeb6dc7b8238d2a8ab5a90e44
Jelte Homminga <jeltehomminga@gmail.com>
2019-01-31 16:38:37 +0100

    small screen

---
+++ starter_code/index.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+    let myLinks = document.getElementById("myLinks");
+    myLinks.style.display === "block" ? myLinks.style.display = "none" : myLinks.style.display = "block";
+  }
