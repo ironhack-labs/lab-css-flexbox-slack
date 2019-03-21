6c5415b617a048457361e2c6e4d50e1566bedd7f
raveBolognesa <rafael.arrieta.gar@gmail.com>
2019-03-21 15:19:27 +0100

    first

---
+++ starter_code/javascript/main.js
@@ -0,0 +1,7 @@
+function classToggle() {
+    const navs = document.querySelectorAll('.Navbar__Items')
+    
+    navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+  }
+  document.querySelector('.Navbar__Link-toggle')
+    .addEventListener('click', classToggle);
