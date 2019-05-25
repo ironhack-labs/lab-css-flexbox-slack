54ebf6cc18d510153e4f31ceb799c70665193d20
tuttifrutsy <yoratita@hotmail.com>
2019-05-25 00:41:30 -0500

    done

---
+++ starter_code/js/app.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
