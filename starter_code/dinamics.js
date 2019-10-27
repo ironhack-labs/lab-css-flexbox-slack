25c543564ee621946f645dbd32d51acf6efa0376
catarinaccmateus <catarina_1102@hotmail.com>
2019-10-27 19:43:11 +0000

    still working on it

---
+++ starter_code/dinamics.js
@@ -0,0 +1,11 @@
+function myFunction() {
+  var x = document.getElementById("myDropdown");
+  if (x.style.display === "inline-block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "inline-block";
+  }
+}
+
+
+  //https://www.w3schools.com/howto/howto_js_dropdown.asp
