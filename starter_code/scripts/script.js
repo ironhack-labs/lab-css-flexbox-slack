86a4f4864c5d2ad3b9b2f5becf677381aed90c3f
Pradeepa Shanmugasundaram <pradeepa@Pradeepas-MacBook-Pro.local>
2019-10-27 23:13:58 -0400

    changed the menu icon

--- starter_code/scripts/script.js
+++ starter_code/scripts/script.js
@@ -1,6 +1,7 @@
 function toggleMenu() {
     const menuList = document.getElementsByClassName('menu-list');
     const dim = document.getElementsByClassName('modal-overlay');
+    const menuIcon = document.getElementsByClassName('menu-icon');
     if(menuList[0].style.display === "none" && dim[0].style.display === "none") {
         menuList[0].style.display = "flex";
         dim[0].style.display = "block";
