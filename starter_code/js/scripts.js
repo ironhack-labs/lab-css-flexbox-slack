7ab46574062d4fc459fee1e65848d3a33dc18f2e
franck <franck.treboit@gmail.com>
2020-02-02 15:44:24 +0100

    version approche 1 slack responsive

---
+++ starter_code/js/scripts.js
@@ -0,0 +1,9 @@
+function deployNav() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
