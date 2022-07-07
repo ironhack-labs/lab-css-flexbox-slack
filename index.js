1d1f84d6b9e025fcd4932b8280f7a9e83de436cd
Matulan Mahenthra <Matu_25@hotmail.com>
2022-07-07 14:19:16 +0100

    adds changes

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+
+// Single line comment
+
+// declation
+
+let welcome;
+
+//Initialzation
+
+welcome = "Hello there"
+
+// Declare and initialize at the same time
+
+let greeting ="Hello Ironhacker"
