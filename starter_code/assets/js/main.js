a13d9fb22538e5e31d72ae039338df7e189bf719
javideas <infojavideas@gmail.com>
2022-12-06 19:30:26 +0200

    first pull request, process

---
+++ starter_code/assets/js/main.js
@@ -0,0 +1,18 @@
+const menuBtn = document.getElementById("menu");
+const dropDown = document.getElementById("dropDown");
+
+menuBtn.onclick = () => {
+  if (dropDown.style.display === 'block') {
+    dropDown.style.display = 'none';
+  } else {
+    dropDown.style.display = 'block';
+  }
+}
+
+document.body.onclick = (ev) => {
+  if (ev.target.id !== 'menu') {
+    dropDown.style.display = 'none';
+  }
+}
+
+<img src="./images/icon-menu.png" alt="Slack logo" height="20px"/>
