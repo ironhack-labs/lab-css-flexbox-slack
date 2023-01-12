8eeb7fbf3a74af8cf86c5ecc5f104957735a4e1b
javideas <infojavideas@gmail.com>
2023-01-12 18:19:20 +0200

    finished

--- starter_code/assets/js/main.js
+++ starter_code/assets/js/main.js
@@ -2,7 +2,7 @@ const menuBtn = document.getElementById("menu");
 const dropDown = document.getElementById("dropDown");
 
 menuBtn.onclick = () => {
-  if (dropDown.style.display === 'block') {
+  if (dropDown.style.display === 'block' && window.innerWidth <= 1024) {
     dropDown.style.display = 'none';
   } else {
     dropDown.style.display = 'block';
@@ -10,7 +10,7 @@ menuBtn.onclick = () => {
 }
 
 document.body.onclick = (ev) => {
-  if (ev.target.id !== 'menu') {
+  if (ev.target.id !== 'menu'  && window.innerWidth <= 1024) {
     dropDown.style.display = 'none';
   }
 }
